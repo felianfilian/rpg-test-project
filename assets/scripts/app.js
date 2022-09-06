108f49c7e852624fda61b90177242e0c0d273dec
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-09-06 09:15:51 +0200

    strong damage

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 
@@ -10,4 +11,12 @@ function dealDamage() {
   monHealth.innerHTML = Math.round(currentMonsterHealth);
 }
 
+function dealStrongDamage() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  monHealth.innerHTML = Math.round(currentMonsterHealth);
+}
+
 attackBtn.addEventListener('click', dealDamage);
+strongAttackBtn.addEventListener('click', dealStrongDamage);
+healBtn.addEventListener('click', dealStrongDamage);
